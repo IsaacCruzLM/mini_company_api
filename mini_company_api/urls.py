a91b3f6314dd9cc6fb96d9260a24561ba451a4d6
IsaacCruzLM <isaac.clm1@gmail.com>
2025-04-23 20:35:24 -0300

    feat: init the project
    init mini company api and init user model witj jwt auth

---
+++ mini_company_api/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('users/', include('users.urls')),
+    path('auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
---
+++ users/views.py
@@ -0,0 +1,9 @@
+from rest_framework import generics, permissions
+from .models import User
+from .serializers import UserSerializer
+
+# Create your views here.
+class UserListView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
