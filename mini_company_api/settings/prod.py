4c95c20a19ff024102037c5a615196e0acdc2c26
IsaacCruzLM <isaac.clm1@gmail.com>
2025-04-25 11:30:29 -0300

    feat: add configs to prod env database
    add configs to conect in the supabase database

---
+++ mini_company_api/settings/prod.py
@@ -0,0 +1,20 @@
+# seuprojeto/settings/prod.py
+from .base import *
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DEBUG = False
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+    }
+}
